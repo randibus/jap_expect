eb88fc7d32666bda519cab795e2df76af947ef81
Randy Isbell <risbell@ihiji.com>
2019-04-13 18:07:06 -0500

    New file hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+extra = ''
+for x in range(0, 20):
+   if x > 10:
+       extra = "more stuff here"
+   else:
+       extra = "bueno"
+   print "We're on time %d %s" % (x,extra)
+
+print "End of Program"
